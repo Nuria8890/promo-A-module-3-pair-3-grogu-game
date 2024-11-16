48cdff4b2ffae955fce44ff7a16a710540ff5773
Nuria Campo <nuria.campo7@gmail.com>
2024-11-16 20:17:17 +0100

    funcion rollDice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,52 @@
 import "../styles/App.scss";
 import Header from "./Header";
-import Main from "./Main";
+import Board from "./Board";
 
 function App() {
+  const dice = ["grogu", "galleta", "huevo", "rana"];
+
+  let diceResult = "";
+
+  function rollDice() {
+    const position = Math.round(Math.random() * (dice.length - 1 - 0) + 0);
+    console.log("position es", position);
+    diceResult = dice[position];
+    console.log("El resultado del dado es", diceResult);
+    return diceResult;
+  }
+
   return (
     <>
       <Header />
-      <Main />
+      <main className="page">
+        <Board />
+
+        <section>
+          <button onClick={rollDice} className="dice">
+            Lanzar Dado
+          </button>
+          <div className="game-status">En curso</div>
+        </section>
+
+        <section className="goods-container">
+          <div className="goods-item">🍪</div>
+          <div className="goods-item">🍪</div>
+          <div className="goods-item">🍪</div>
+        </section>
+        <section className="goods-container">
+          <div className="goods-item">🥚</div>
+          <div className="goods-item">🥚</div>
+          <div className="goods-item">🥚</div>
+        </section>
+        <section className="goods-container">
+          <div className="goods-item">🐸</div>
+          <div className="goods-item">🐸</div>
+          <div className="goods-item">🐸</div>
+        </section>
+        <section>
+          <button className="restart-button">Reiniciar Juego</button>
+        </section>
+      </main>
     </>
   );
 }
